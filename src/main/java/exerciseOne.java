91c2c13a506702c412da1e250a539a96dc132e5b
projekt1701 <tassilo.g@gmx.net>
2023-05-31 21:44:39 +0200

    add a method that print numbers in a special way

--- src/main/java/exerciseOne.java
+++ src/main/java/exerciseOne.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
 public class exerciseOne {
+    //TODO Warum muss dieses Klassen Array static sein?
+    static String [] wordsOfNumbers = {"Null","Eins", "Zwei", "Drei", "Vier", "Fünf", "Sechs", "Sieben", "Acht", "Neun"};
+
     public static void main(String[] args) {
 
 
-        printUserName();
-        System.out.println(calculatePowerOfInt(9));
+      //  printUserName();
 
+        //System.out.println(calculatePowerOfInt(9));
+
+      //  System.out.println(determineWordOfNumber(4));
+
+        printNumbers();
     }
 
     public static void printUserName(){
@@ -21,5 +28,35 @@ public class exerciseOne {
 
     public static double calculatePowerOfInt(int num){
         return Math.pow(num, 2);
+    }
+
+    public static String determineWordOfNumber(int num){
+        //TODO numWord musste initialisiert werden ansonsten gab es eine Fehlermeldung, unten bei return numWord!?
+        String numWord = null;
+
+        for (int i = 0; i <= 9; i++){
+
+            if(num == i){
+                numWord = wordsOfNumbers[i];
+            }
+        }
+        return numWord;
+
+    }
+
+    public static void printNumbers(){
+
+        for(int i = 1; i <= 100; i++){
+
+            if (i%5 == 0 && i%3 == 0){
+                System.out.println("#$" + i);
+            }else if (i%5 == 0){
+                System.out.println("$" + i);
+            }else if (i%3 == 0){
+                System.out.println("#" + i);
+            }else {
+                System.out.println(i);
+            }
+        }
     }
 }
