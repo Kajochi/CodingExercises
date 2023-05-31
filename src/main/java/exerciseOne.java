244c8d66fa1cb0cd4a2c4ce99febab876035ea9d
projekt1701 <tassilo.g@gmx.net>
2023-05-31 20:57:10 +0200

    outsource code into a method

--- src/main/java/exerciseOne.java
+++ src/main/java/exerciseOne.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class exerciseOne {
     public static void main(String[] args) {
 
+
+        printUserName();
+
+
+    }
+
+    public static void printUserName(){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hi, bitte gib deinen Namen ein");
+        System.out.println("Hi, bitte gib deinen Namen ein:");
 
         String name = scanner.nextLine();
 
         System.out.println("Hallo " + name + ". Schön das du da bist!");
-
     }
 }
